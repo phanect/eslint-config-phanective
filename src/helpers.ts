6adc9e57781b1dd71e1cc718e1a00fa8efbdeab5
Jumpei Ogawa <git@phanective.org>
2023-12-30 23:43:57 +0900

    refactor: move `mergeConfigs()` to test/configs.test.ts
    `mergeConfigs()` is no-longer used in production code. It is only used by configs.test.ts now.
    Therefore, I move it to configs.test.ts so that I can move lodash from `dependencies` to `devDependencies`.
    
    NOTE: `mergeConfigs()` is planned to be replaced by `deepmerge`.

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,7 +1,5 @@
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
-import mergeWith from "lodash.mergewith";
-import cloneDeep from "lodash.clonedeep";
 import type { Linter } from "eslint";
 
 export const toTSRules = (jsRules: Linter.RulesRecord): Linter.RulesRecord => {
@@ -12,17 +10,6 @@ export const toTSRules = (jsRules: Linter.RulesRecord): Linter.RulesRecord => {
   }
 
   return tsRules;
-};
-
-
-export const mergeConfigs = (config1, config2) => {
-  const _config1 = cloneDeep(config1);
-
-  return mergeWith(_config1, config2, (a, b) => {
-    if (Array.isArray(a) && Array.isArray(b)) {
-      return a.concat(b);
-    }
-  });
 };
 
 // If you want to get project root on e.g. src/configs/overrides/lang-spacific.ts,
