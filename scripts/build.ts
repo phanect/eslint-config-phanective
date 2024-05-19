b98ac541e73298de7b0899e0644ceb9968701616
Jumpei Ogawa <git@phanective.org>
2024-05-19 12:11:01 +0900

    refactor: generate ESLint configs under dist/

--- scripts/build.ts
+++ scripts/build.ts
@@ -14,20 +14,20 @@ import vueTS from "../src/configs/vue+ts.ts";
 import withDeps from "../src/configs/with-deps.ts";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
-const rootDir = join(__dirname, "../");
+const distDir = join(__dirname, "../dist");
 
-await mkdir(rootDir, { recursive: true });
+await mkdir(distDir, { recursive: true });
 await Promise.all([
-  writeFile(join(rootDir, "plain.json"), JSON.stringify(plain, null, 2)),
-  writeFile(join(rootDir, "node.json"), JSON.stringify(node, null, 2)),
-  writeFile(join(rootDir, "svelte.json"), JSON.stringify(svelte, null, 2)),
-  writeFile(join(rootDir, "react.json"), JSON.stringify(react, null, 2)),
-  writeFile(join(rootDir, "next.json"), JSON.stringify(next, null, 2)),
-  writeFile(join(rootDir, "vue+js.json"), JSON.stringify(vueJS, null, 2)),
-  writeFile(join(rootDir, "vue+ts.json"), JSON.stringify(vueTS, null, 2)),
-  writeFile(join(rootDir, "nuxt+js.json"), JSON.stringify(nuxtJS, null, 2)),
-  writeFile(join(rootDir, "nuxt+ts.json"), JSON.stringify(nuxtTS, null, 2)),
-  writeFile(join(rootDir, "with-deps.json"), JSON.stringify(withDeps, null, 2)),
+  writeFile(join(distDir, "plain.json"), JSON.stringify(plain, null, 2)),
+  writeFile(join(distDir, "node.json"), JSON.stringify(node, null, 2)),
+  writeFile(join(distDir, "svelte.json"), JSON.stringify(svelte, null, 2)),
+  writeFile(join(distDir, "react.json"), JSON.stringify(react, null, 2)),
+  writeFile(join(distDir, "next.json"), JSON.stringify(next, null, 2)),
+  writeFile(join(distDir, "vue+js.json"), JSON.stringify(vueJS, null, 2)),
+  writeFile(join(distDir, "vue+ts.json"), JSON.stringify(vueTS, null, 2)),
+  writeFile(join(distDir, "nuxt+js.json"), JSON.stringify(nuxtJS, null, 2)),
+  writeFile(join(distDir, "nuxt+ts.json"), JSON.stringify(nuxtTS, null, 2)),
+  writeFile(join(distDir, "with-deps.json"), JSON.stringify(withDeps, null, 2)),
 ]);
 
 console.log("JSONs are successfully generated");
